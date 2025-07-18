9b55768814000ea23ea4ecfb885d8d585f685024
Dannissimo20 <Denicha_d_89@mail.ru>
2025-07-18 12:55:18 +0300

    Move "get_records" on separate router

--- src/app/main.py
+++ src/app/main.py
@@ -1,23 +1,10 @@
-from fastapi import FastAPI, Depends    
-
-from src.app.database.database import DBWriter
-from src.app.repositories.record_repo import RecordRepo
-from src.app.schemas.record_schema import RecordOut
+from fastapi import FastAPI
+from src.app.api.records import record_router
 
 app: FastAPI = FastAPI()
 
-db_writer: DBWriter = DBWriter()
-
-
-def get_record_repo() -> RecordRepo:
-    return RecordRepo(db_writer)
-
+app.include_router(record_router)
 
 @app.get("/")
 def healthcheck():
-    return 200
-
-
-@app.get("/get_records", response_model=list[RecordOut])
-def get_all(record_repo: RecordRepo = Depends(get_record_repo)):
-    return record_repo.get_records()
+    return {"status": "ok", "message": "Service is running"}
