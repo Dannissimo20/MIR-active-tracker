4a6620b28b9fc212775e23511a044e3d764e41f0
Dannissimo20 <Denicha_d_89@mail.ru>
2025-07-21 16:08:52 +0300

    Kebab-case and prefix fix

--- src/app/api/records.py
+++ src/app/api/records.py
@@ -12,38 +12,39 @@ def get_record_repo() -> RecordRepo:
 
 
 record_router = APIRouter(
+    prefix="/record",
     tags=['Record']
 )
 
 
-@record_router.get("/all_records", response_model=list[RecordOut])
+@record_router.get("/all-records", response_model=list[RecordOut])
 def get_all(record_repo: RecordRepo = Depends(get_record_repo)):
     return record_repo.get_all()
 
 
-@record_router.get("/all_active_records", response_model=list[RecordOut])
+@record_router.get("/all-active-records", response_model=list[RecordOut])
 def get_all_active(record_repo: RecordRepo = Depends(get_record_repo)):
     return record_repo.get_active_records()
 
 
-@record_router.get("/record", response_model=RecordOut)
+@record_router.get("", response_model=RecordOut)
 def get_record_by_id(id: UUID4, record_repo: RecordRepo = Depends(get_record_repo)):
     return record_repo.get_by_id(id)
 
 
-@record_router.post("/record", response_model=list[RecordOut])
+@record_router.post("", response_model=list[RecordOut])
 def create_record(request: RecordIn, record_repo: RecordRepo = Depends(get_record_repo)):
     result = record_repo.create(request)
     return result
 
 
-@record_router.patch("/record", response_model=list[RecordOut])
+@record_router.patch("", response_model=list[RecordOut])
 def update_record(id: UUID4, request: RecordUpdate, record_repo: RecordRepo = Depends(get_record_repo)):
     result = record_repo.update(id, request)
     return result
 
 
-@record_router.delete("/record", response_model=list[RecordOut])
+@record_router.delete("", response_model=list[RecordOut])
 def cancel_record(id: UUID4, record_repo: RecordRepo = Depends(get_record_repo)):
     result = record_repo.cancel_record(id)
     return result
